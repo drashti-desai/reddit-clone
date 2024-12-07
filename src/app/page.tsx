5be7d92ab5f5b28159e2261f4b792e278b844910
unknown <desaidrashti5@gmail.com>
2024-12-07 14:38:19 -0600

    setting up Next.js & Tailwind CSS

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title> Reddit Clone </title>
+      </Head>
+      
+      <h1>Hello Users</h1>
+    </div>
+  );
+}
